65b0532a98d49b8ea6def4c0f8a61a564a62b43e
NataliaPonomarevaMM <natpon12@gmail.com>
2020-09-14 11:33:29 +0300

    Updates

---
+++ precache-manifest.2ca0182692388a36cb237ec5e6511fb2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0940839ea3a0bc434bfd07a3acf6588d",
+    "url": "/vk-mini-app/index.html"
+  },
+  {
+    "revision": "0ef793d1b6cf974e0e8e",
+    "url": "/vk-mini-app/static/css/2.09e62520.chunk.css"
+  },
+  {
+    "revision": "0257911096a8353044bf",
+    "url": "/vk-mini-app/static/css/main.e2cee04f.chunk.css"
+  },
+  {
+    "revision": "0ef793d1b6cf974e0e8e",
+    "url": "/vk-mini-app/static/js/2.e6fb0622.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/vk-mini-app/static/js/2.e6fb0622.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0257911096a8353044bf",
+    "url": "/vk-mini-app/static/js/main.93323e3e.chunk.js"
+  },
+  {
+    "revision": "bd98e19e4001ef065cdc",
+    "url": "/vk-mini-app/static/js/runtime-main.92f3644c.js"
+  }
+]);
